ee25ddb7984d9acd1210476075079f0d903017ec
Brian Faust <hello@basecode.sh>
2019-04-01 12:03:45 +0300

    refactor(core-logger-console): adjust to latest core-logger changes

--- packages/core-logger-console/src/driver.ts
+++ packages/core-logger-console/src/driver.ts
@@ -1,54 +1,18 @@
+import { Logger } from "@arkecosystem/core-interfaces";
 import { AbstractLogger } from "@arkecosystem/core-logger";
-import isEmpty from "lodash.isempty";
-import { inspect } from "util";
 
 export class ConsoleLogger extends AbstractLogger {
-    public logger: Console;
-    public silent: boolean = false;
+    protected logger: Console;
 
-    public make() {
+    public make(): Logger.ILogger {
         this.logger = console;
 
         return this;
     }
 
-    public error(message: any): void {
-        this.createLog("error", message);
-    }
-
-    public warn(message: any): void {
-        this.createLog("warn", message);
-    }
-
-    public info(message: any): void {
-        this.createLog("info", message);
-    }
-
-    public debug(message: any): void {
-        this.createLog("debug", message);
-    }
-
-    public verbose(message: any): void {
-        this.createLog("trace", message);
-    }
-
-    public suppressConsoleOutput(suppress: boolean): void {
-        this.silent = suppress;
-    }
-
-    private createLog(method: string, message: any): void {
-        if (this.silent) {
-            return;
-        }
-
-        if (isEmpty(message)) {
-            return;
-        }
-
-        if (typeof message !== "string") {
-            message = inspect(message, { depth: 1 });
-        }
-
-        this.logger[method](message);
+    protected getLevels(): Record<string, string> {
+        return {
+            verbose: "trace",
+        };
     }
 }
--- packages/core-logger-console/src/plugin.ts
+++ packages/core-logger-console/src/plugin.ts
@@ -1,5 +1,5 @@
 import { Container } from "@arkecosystem/core-interfaces";
-import { LogManager } from "@arkecosystem/core-logger";
+import { LoggerManager } from "@arkecosystem/core-logger";
 import { ConsoleLogger } from "./driver";
 
 export const plugin: Container.PluginDescriptor = {
@@ -7,25 +7,6 @@ export const plugin: Container.PluginDescriptor = {
     alias: "logger",
     extends: "@arkecosystem/core-logger",
     async register(container: Container.IContainer, options) {
-        const logManager: LogManager = container.resolvePlugin("log-manager");
-        await logManager.makeDriver(new ConsoleLogger(options));
-
-        const driver = logManager.driver();
-        driver.debug(`Data Directory => ${process.env.CORE_PATH_DATA}`);
-        driver.debug(`Config Directory => ${process.env.CORE_PATH_CONFIG}`);
-
-        if (process.env.CORE_PATH_CACHE) {
-            driver.debug(`Cache Directory => ${process.env.CORE_PATH_CACHE}`);
-        }
-
-        if (process.env.CORE_PATH_LOG) {
-            driver.debug(`Log Directory => ${process.env.CORE_PATH_LOG}`);
-        }
-
-        if (process.env.CORE_PATH_TEMP) {
-            driver.debug(`Temp Directory => ${process.env.CORE_PATH_TEMP}`);
-        }
-
-        return driver;
+        return container.resolvePlugin<LoggerManager>("log-manager").createDriver(new ConsoleLogger(options));
     },
 };
